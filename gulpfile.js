c8f9b6e276c9a61db41333d2f1766ef2b0f517ae
Afiq Hafizuddin <hafizuddinafiq@gmail.com>
2022-04-17 22:42:04 +0800

    restucture the project

--- gulpfile.js
+++ gulpfile.js
@@ -4,11 +4,11 @@ const { src, dest, watch, series } = require("gulp");
 const sass = require("gulp-sass")(require("sass"));
 
 function buildStyles() {
-  return src("*.scss").pipe(sass()).pipe(dest("css"));
+  return src("library/**/*.scss").pipe(sass()).pipe(dest("css"));
 }
 
 function watchTask() {
-  watch(["*.scss"], buildStyles);
+  watch(["library/**/*.scss"], buildStyles);
 }
 
 exports.default = series(buildStyles, watchTask);
